a0c2058e6f69ffe9349bb2819f418d0f7711255c
Симкин Максим <maximustdie@yandex.ru>
2023-03-22 17:04:34 +0300

    Добавил конфиг

---
+++ backend/src/core/config.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+load_dotenv('.env')
+
+
+class Postgres(BaseSettings):
+    USER: str = "user"
+    PASSWORD: str = "123"
+    DB: str = "db"
+    HOST: str = "localhost"
+    PORT: str = "5432"
+
+    class Config:
+        env_prefix = 'POSTGRES_'
+
+    def build_url(self) -> str:
+        return f'postgresql+asyncpg://' \
+               f'{self.USER}:{self.PASSWORD}@' \
+               f'{self.HOST}:{self.PORT}/{self.DB}'
+
+
+class Settings(BaseSettings):
+    POSTGRES: Postgres = Postgres()
+
+
+settings = Settings()
